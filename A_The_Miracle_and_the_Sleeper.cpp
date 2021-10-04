c4101afa26c6338a5f255805121d110631c02451
maggied07 <91803339+maggied07@users.noreply.github.com>
2021-10-04 15:36:57 +0530

    Add files via upload

---
+++ A_The_Miracle_and_the_Sleeper.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+#define IOS ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+ 
+int main()
+{
+    IOS;
+    ll t;
+    cin>>t;
+    while(t--)
+    {
+        ll a,b;
+        cin>>a>>b;
+        if(a>ceil(b/2.0))
+        {
+            cout<<b-a<<endl;
+        }
+        else
+        {
+            if(b%2!=0)
+            {
+                ll x=(b-1)/2;
+                ll y=b-x-1;
+                cout<<y<<endl;
+            }
+            else
+            {
+                ll x=b/2+1;
+                cout<<b-x<<endl;
+            }
+        }
+    }
+}
