5ae955953bbfbab4d83983849654a01eb08923d0
kk091109 <91882038+kk091109@users.noreply.github.com>
2021-10-04 15:57:19 +0530

    Update A_The_Miracle_and_the_Sleeper.cpp

--- A_The_Miracle_and_the_Sleeper.cpp
+++ A_The_Miracle_and_the_Sleeper.cpp
@@ -1,7 +1,7 @@
 
 #include<bits/stdc++.h>
 using namespace std;
-typedef long long int ll;
+#define ll long long int;
 #define IOS ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
  
 int main()
